bbd6bf371ac8103261cfcdd8bfc880aa8e201a9e
Akira Hayashi <rindrics@gmail.com>
2024-10-27 07:53:18 +0900

    Receive args filename and email

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+
+#[command(name = "ppap")]
+#[command(author = "Rindrics")]
+#[command(version = "0.1.0")]
+#[command(about = "CLI tool that uses traditional Japanese file sharing protocol")]
+
+struct Opts {
+    #[arg(help = "Target file to be sent")]
+    file: String,
+
+    #[arg(help = "Email address of the recipient")]
+    email: String,
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+    println!("ppnp (file: {}, email: {})", opts.file, opts.email);
+}
